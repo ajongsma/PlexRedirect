a90c861cb3f3d78fd388e3990d5b8ea02e8e2007
ajongsma <a.jongsma@gmail.com>
2017-08-21 19:35:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -145,19 +145,6 @@
 					<p>Powered by PlexPy.</p>
 				</a>
 			</div>
-			
-			<div class="col-lg-4">
-				<?php if (strlen($SERVER_STATS_URL) > 0) { ?>
-					<a href="//<?=$SERVER_STATS_URL?>" target="_top">
-						<img src="assets/img/server.svg" width="180" alt="">
-						<h4>Server Stats</h4>
-						<p>Graphical Server Information Including CPU, Network, Harddrive and Memory.</p>
-					</a>
-				<?php } elseif ($SERVER_STATS_DESATURATE == True) { ?>
-					<img src="assets/img/server.svg" width="180" class="desaturate" alt="">
-					<h4>Server stats, coming soon!</h4>
-				<?php } ?>
-			</div>
 
 			
 			
